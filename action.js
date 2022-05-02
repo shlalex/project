569b3fc8beda530f4ee5aa9b0989a198b9d8a553
Alex <example@gmail.com>
2022-05-02 13:40:59 +0300

    first

--- action.js
+++ action.js
@@ -1,1 +1,19 @@
-console.log(2+2*2 !=="6");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",""); //+ перед promt сделал из string в number
+
+const personalMovieDB = {
+    count:numberOfFilms, //занесли переменную через в переменную через двоеточие ":"
+    movies: {}, //занесли в переменную пустой обьект "{}"
+    actors: {},
+    genres: [], //поместили пустой массив []
+    privat:false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+b = prompt("На сколько оцените его?", ""),
+c = prompt("Один из последних просмотренных фильмов?", ""),
+d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log (personalMovieDB);
--- app.js
+++
@@ -1,1 +0,0 @@
-//var category = "toys";
